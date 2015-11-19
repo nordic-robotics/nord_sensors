007495b8ec8d627a53f3c271e460cb6c6c875fd4
Forrest Gump <vahiki123@gmail.com>
2015-11-19 12:20:04 +0100

    back left sensor changed

--- src/ir_sensors.cpp
+++ src/ir_sensors.cpp
@@ -3,29 +3,27 @@
 #include "nord_messages/IRSensors.h"
 #include <cmath>
 
-template<class T>
-
 float front_inverse(int x){
-		return (-40.19*exp(x*(-0.004891))+41.03*exp(x*(-0.004897)));
+		return (-40.19*std::exp(x*(-0.004891))+41.03*std::exp(x*(-0.004897)));
 }
 float back_inverse(int x){
-		return (0.05347*exp(x*(0.0004147))+1.9*exp(x*(-0.008829)));
+		return (0.05347*std::exp(x*(0.0004147))+1.9*std::exp(x*(-0.008829)));
 }
 
 float lfront_inverse(int x){
-		return (0.04444*exp(x*(0.001731))+3.319*exp(x*(-0.01844)));
+		return (28.73*std::exp(x*(-0.04203))+0.4839*std::exp(x*(-0.006104)));
 }
 
 float lback_inverse(int x){
-		return (77.26*exp(x*(-0.04616))+0.5006*exp(x*(-0.005953)));
+		return (25.57*std::exp(x*(-0.03562))+0.2736*std::exp(x*(-0.003856)));
 }
 
 float rback_inverse(int x){
-		return (0.3986*exp(x*(-0.04507))+0.515*exp(x*(-0.00663)));
+		return (0.3986*std::exp(x*(-0.04507))+0.515*std::exp(x*(-0.00663)));
 }
 
 float rfront_inverse(int x){
-		return (0.4628*exp(x*(-0.1022))+0.515*exp(x*(-0.00663)));
+		return (0.4628*std::exp(x*(-0.1022))+0.515*std::exp(x*(-0.00663)));
 }
 
 int main(int argc, char** argv)
@@ -44,30 +42,34 @@ int main(int argc, char** argv)
 			ir.back = back_inverse(msg->ch5);
 			
 			ir.front = front_inverse(msg->ch6);
+/*ir.back = back_inverse(0);
+			
+			ir.front = front_inverse(0);*/
 			 
 			if(msg->ch1>320){
 				ir.left_front = lfront_inverse(0);
 			}else{
 				ir.left_front = lfront_inverse(msg->ch1);
 			}
-			
+			//ir.left_front = lfront_inverse(0);
 			if(msg->ch3>320){
 				ir.left_back = lback_inverse(0);
 			}else{
 				ir.left_back = lback_inverse(msg->ch3);
 			}
-			
+			//ir.left_back = lback_inverse(0);
 			if(msg->ch7>400){
 				ir.right_back = rback_inverse(0);
 			}else{
 				ir.right_back = rback_inverse(msg->ch7);
 			}
-			
+			//ir.right_back = rback_inverse(0);
 			if(msg->ch8>320){
 				ir.right_front = rfront_inverse(0);
 			}else{
 				ir.right_front = rfront_inverse(msg->ch8);
 			}
+			//ir.right_front = rfront_inverse(0);
             ir_pub.publish(ir);
         });
 
