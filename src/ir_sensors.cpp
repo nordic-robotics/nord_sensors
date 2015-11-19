7b3636c46141a79323ab8b276e1cfb1453fd0b5e
Forrest Gump <vahiki123@gmail.com>
2015-11-19 23:46:37 +0100

    debug

--- src/ir_sensors.cpp
+++ src/ir_sensors.cpp
@@ -20,23 +20,23 @@ int main(int argc, char** argv)
 
     auto lorentz1 = make_lorentz(0.337f, 107.55f, 43.76f);
     auto lorentz2 = make_lorentz(0.479f, 140.18f, 231.33f);
-    auto back_lorentz = [&](float x) { return lorentz1(std::max(x, 110)) + lorentz2(std::max(x, 110)) + 0.003; };
+    auto back_lorentz = [&](float x) { return lorentz1(std::max(x, 110.0f)) + lorentz2(std::max(x, 110.0f)) + 0.003f; };
 
     auto lorentz3 = make_lorentz(0.4157f, 109.9f, 68.31f);
-    auto front_lorentz = [&](float x) { return lorentz3(std::max(x, 110)); };
+    auto front_lorentz = [&](float x) { return lorentz3(std::max(x, 110.0f)); };
 	
 	auto lorentz5 = make_lorentz(0.42f, -1.157f, 29.7f);
     auto lorentz6 = make_lorentz(0.227f, 29.78f, 197.32f);
-    auto Rback_lorentz = [&](float x) { return lorentz5(x) + lorentz6(x) + 0.085; };
+    auto Rback_lorentz = [&](float x) { return lorentz5(x) + lorentz6(x) + 0.085f; };
 
     auto lorentz4 = make_lorentz(0.864f, 80.48f, 59.85f);
-    auto Lfront_lorentz = [&](float x) { return lorentz4(std::max(x, 80)) + 0.097; };
+    auto Lfront_lorentz = [&](float x) { return lorentz4(std::max(x, 80.0f)) + 0.097f; };
 	
 	auto lorentz7 = make_lorentz(0.569f, 101.96f, 62.96f);
-    auto Lback_lorentz = [&](float x) { return lorentz7(std::max(x, 100)) + 0.1; };
+    auto Lback_lorentz = [&](float x) { return lorentz7(std::max(x, 100.0f)) + 0.1f; };
 	
 	auto lorentz8 = make_lorentz(132.3f, -77.17f, 12.35f);
-    auto Rfront_lorentz = [&](float x) { return lorentz8(x) +0.09; };
+    auto Rfront_lorentz = [&](float x) { return lorentz8(x) +0.09f; };
 	
 
     ros::Publisher ir_pub = n.advertise<IRSensors>("/nord/sensors/ir", 10);
