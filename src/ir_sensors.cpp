109ac5a5d7f9529b48291b162ea10fede9297e0b
Forrest Gump <vahiki123@gmail.com>
2015-11-23 19:43:00 +0100

    after nuc failure

--- src/ir_sensors.cpp
+++ src/ir_sensors.cpp
@@ -11,7 +11,7 @@ float ir_left_back_temp;
 float ir_right_back_temp;
 float ir_right_front_temp;
 
-const float ALPHA=0.15f;
+const float ALPHA=0.5f;
 
 template<class T>
 std::function<T(T)> make_lorentz(T height, T center, T hwhm)
@@ -37,15 +37,15 @@ int main(int argc, char** argv)
     auto front_lorentz = [&](float x) { return lorentz3(x) + lorentz9(x); };
 
 	
-    auto lorentz5 = make_lorentz(0.42f, -1.157f, 14.85f);
-    auto lorentz6 = make_lorentz(0.227f, 29.78f, 98.66f);
-    auto Rback_lorentz = [&](float x) { return lorentz5(x) + lorentz6(x) + 0.085f; };
+    auto lorentz5 = make_lorentz(0.6607f, -36.32f, 131.3f);
+   // auto lorentz6 = make_lorentz(0.227f, 29.78f, 98.66f);
+    auto Rback_lorentz = [&](float x) { return lorentz5(std::max(x, 20.0f)) -0.1293+std::max(x, 20.0f)*0.000166 + 0.085f; };
 
-    auto lorentz4 = make_lorentz(0.4866f, 92.815f, 38.06f);
-    auto Lfront_lorentz = [&](float x) { return lorentz4(std::max(x, 105.0f)) + 0.097f; };
+    auto lorentz4 = make_lorentz(0.963f, 19.24f, 97.91f);
+    auto Lfront_lorentz = [&](float x) { return lorentz4(std::max(x, 75.0f))-0.333f+0.000761*std::max(x, 75.0f) + 0.097f; };
 	
-    auto lorentz7 = make_lorentz(0.4147f, 110.039f, 34.10f);
-    auto Lback_lorentz = [&](float x) { return lorentz7(std::max(x, 120.0f)) + 0.1f; };
+    auto lorentz7 = make_lorentz(0.3448f, 95.51f, 25.52f);
+    auto Lback_lorentz = [&](float x) { return lorentz7(std::max(x, 100.0f))+0.09874-0.0003979*std::max(x, 100.0f) + 0.1f; };
 	
     auto lorentz8 = make_lorentz(127.31f, -77.53f, 6.32f);
     auto Rfront_lorentz = [&](float x) { return lorentz8(x) +0.09f; };
